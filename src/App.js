31d3156179edabd33428ed1c87500ce02c0c31e5
Jaidha Rosenblatt <jrr59@duke.edu>
2020-07-04 08:19:37 -0700

    set up routing and installed antd and router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import RandomChooser from "./pages/RandomChooser";
+import AddRestaurant from "./pages/AddRestaurant";
+import Restaurants from "./pages/Restaurants";
 
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/random" exact component={RandomChooser} />
+          <Route path="/add" exact component={AddRestaurant} />
+          <Route path="/all" exact component={Restaurants} />
+          <Redirect to="/random" />;
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/RandomChooser.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RandomChooser = () => {
+  return <div>I'm the chooser</div>;
+};
+
+export default RandomChooser;
