7554a0760a3fe110879f8d69cd563ea531594a52
Jaidha Rosenblatt <jrr59@duke.edu>
2020-12-19 13:23:57 -0800

    created user model from boilerplate

---
+++ server/models/user.js
@@ -0,0 +1,87 @@
+const mongoose = require("mongoose");
+const brcypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const Task = require("./task");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign(
+    { _id: user._id.toString() },
+    process.env.TOKEN_SECRET
+  );
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
+
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
+userSchema.statics.findByCredentials = async (username, password) => {
+  const user = await User.findOne({ username });
+  if (!user) {
+    throw new Error("Unable to log in");
+  }
+
+  const isMatch = await brcypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Unable to log in");
+  }
+
+  return user;
+};
+
+// Hash the plain text password before saving
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await brcypt.hash(user.password, 8);
+  }
+  next();
+});
+
+// Delete user tasks when user is removed
+userSchema.pre("remove", async function (next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
