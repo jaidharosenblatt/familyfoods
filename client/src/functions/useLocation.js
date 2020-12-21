62e012c48ecfff6298a6592a044641bdf9b6b6ff
Jaidha Rosenblatt <jrr59@duke.edu>
2020-12-21 14:22:16 -0800

    added location hook

---
+++ client/src/functions/useLocation.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+/**
+ * Get location of the current user
+ * @returns {Location} matching backend model
+ */
+const useLocation = () => {
+  const [location, setLocation] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const cords = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+      setLocation(cords);
+    });
+  }, []);
+  return [location];
+};
+
+export default useLocation;
