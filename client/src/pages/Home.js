ad9c0c3fc392df9a5df5ff134bf965a7ebf4e9e3
Jaidha Rosenblatt <jrr59@duke.edu>
2020-12-27 16:15:02 -0800

    floating filters panel on desktop

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,29 +1,22 @@
 import React from "react";
 import Restaurants from "../components/restaurants/Restaurants";
-import { Col, Row } from "antd";
 import FilterPane from "../components/filters/FilterPane";
 import FilterPaneModal from "../components/filters/FilterPaneModal";
+import "./pages.css";
 
 const Home = () => {
-  const header = (
-    <>
-      <h1>Restaurants</h1>
-      <p>Find where you should eat</p>
-    </>
-  );
   return (
-    <div className="home-body">
-      <Row gutter={24}>
-        <Col xs={0} md={6}>
-          {header}
-          <FilterPane />
-        </Col>
+    <div className="home">
+      <div className="floating-pane">
+        <h1>Restaurants</h1>
+        <p>Find where you should eat</p>
+        <FilterPane />
+      </div>
 
-        <Col xs={24} md={18}>
-          <FilterPaneModal />
-          <Restaurants />
-        </Col>
-      </Row>
+      <div className="items">
+        <FilterPaneModal />
+        <Restaurants />
+      </div>
     </div>
   );
 };
