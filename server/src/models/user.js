aab299e0c9b0ffb38e5984a293a57ab0e7d30d63
Jaidha Rosenblatt <jrr59@duke.edu>
2020-12-29 20:22:55 -0800

    set Cookie sameSite policy to none

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -71,7 +71,8 @@ userSchema.methods.setJWTCookie = async function (req, res) {
 
   res.cookie("JWT", token, {
     httpOnly: true,
-    sameSite: "strict",
+    path: "/",
+    sameSite: "None",
     expires: new Date(new Date().getTime() + 604800000), // one week
   });
 };
